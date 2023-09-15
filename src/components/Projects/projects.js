4789b37a213aae771cec1e9392cc976772fa7648
Andrew Escudero <boundlesscreations923@gmail.com>
2023-09-15 15:05:01 -0400

    paint-app

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './projects.css'
 import muuse from '../../assets/muuse.png';
 import yes from '../../assets/yes.png';
-
+import paint from '../../assets/paint.png';
 function Projects() {
     return (
         <div className="projects">
@@ -11,8 +11,11 @@ function Projects() {
           </div>
           <div className="projects-img">
             <img src={yes} alt="img" />
+          </div>
+          <div className="projects-img">
+            <img src={paint} alt="img" />
+          </div>
         </div>
-      </div>
     );
   }
   
